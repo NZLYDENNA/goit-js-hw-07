6135d7e2e79a16c12d5a76bfa2f57e0fc9d610c5
NZLYDENNA <166715874+NZLYDENNA@users.noreply.github.com>
2024-10-21 19:02:57 +0200

    61

--- js/task-6.js
+++ js/task-6.js
@@ -3,3 +3,38 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("#controls input");
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const boxesContainer = document.querySelector("#boxes");
+
+function createBoxes(amount) {
+  const elements = [];
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement("div");
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    box.style.margin = "5px";
+    elements.push(box);
+    size += 10;
+  }
+  boxesContainer.append(...elements);
+}
+function destroyBoxes() {
+  boxesContainer.innerHTML = "";
+}
+
+createBtn.addEventListener("click", () => {
+  const amount = parseInt(input.value.trim());
+  if (amount >= 1 && amount <= 100) {
+    destroyBoxes();
+    createBoxes(amount);
+    input.value = "";
+  } else {
+    alert("Please enter a number between 1 and 100");
+  }
+});
+destroyBtn.addEventListener("click", destroyBoxes);
